183d60da38ec1d353747329406a79170465f203b
shunsaku0621 <hamagi.0621@icloud.com>
2022-05-01 11:44:57 +0900

    課題提出

--- src/Chapter10.java
+++ src/Chapter10.java
@@ -12,6 +12,33 @@ public class Chapter10 {
         list.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く", false));
 
         // 以下記述
+        List<Task> incomplete = new ArrayList<>();
+        for(Task task : list) {
+        	if(!task.isDone()) {
+        		incomplete.add(task);
+        	}
+        }
+        
+        System.out.println("未完了のタスクの個数は" + incomplete.size());
+        list.stream() 
+        .filter(f -> !f.isDone())
+		.sorted((f1, f2) -> f1.getDate().compareTo(f2.getDate()))
+		.forEach(System.out::println); 
+        
+//        
+//       
+//      
+//        incomplete.sort(new Comparator<Task>() {
+//          	 public int compare(Task t1, Task t2) {
+//          	   return t1.getDate().compareTo(t2.getDate());
+//          	 }
+//          });
+//        
+//        System.out.println("未完了のタスクの個数は" + incomplete.size());
+//        System.out.println("【未完了のタスクを昇順に並び替えて一覧表示】");
+//        for(Task task : incomplete) {
+//    	    System.out.println(task);
+//        }
         
     }
 }
