5f7d8df746e11505caf1e5b362d274b9865428c8
shunsaku0621 <hamagi.0621@icloud.com>
2022-05-02 11:28:42 +0900

    再提出1

--- src/Chapter10.java
+++ src/Chapter10.java
@@ -10,35 +10,22 @@ public class Chapter10 {
         list.add(new Task(LocalDate.of(2021, 12, 4), "手帳を買う", true));
         list.add(new Task(LocalDate.of(2021, 8, 10), "散髪に行く", false));
         list.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く", false));
-
-        // 以下記述
-        List<Task> incomplete = new ArrayList<>();
-        for(Task task : list) {
-        	if(!task.isDone()) {
-        		incomplete.add(task);
-        	}
-        }
+//
+//        // 以下記述
+        long incompleteCount = list.stream()
+        .filter(f -> !f.isDone())
+        .count();
+        System.out.println("未完了のタスクの個数は" + incompleteCount);
         
-        System.out.println("未完了のタスクの個数は" + incomplete.size());
+        System.out.println("【未完了のタスクを昇順に並び替えて一覧表示】");
+        
         list.stream() 
         .filter(f -> !f.isDone())
-		.sorted((f1, f2) -> f1.getDate().compareTo(f2.getDate()))
+//		.sorted((f1, f2) -> f1.getDate().compareTo(f2.getDate()))
+        .sorted()
 		.forEach(System.out::println); 
         
-//        
-//       
-//      
-//        incomplete.sort(new Comparator<Task>() {
-//          	 public int compare(Task t1, Task t2) {
-//          	   return t1.getDate().compareTo(t2.getDate());
-//          	 }
-//          });
-//        
-//        System.out.println("未完了のタスクの個数は" + incomplete.size());
-//        System.out.println("【未完了のタスクを昇順に並び替えて一覧表示】");
-//        for(Task task : incomplete) {
-//    	    System.out.println(task);
-//        }
+
         
     }
 }
